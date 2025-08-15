3383b01c049de896504a3d75d75285d8849bb245
universe9329-sys <universe9329@gmail.com>
2025-08-16 01:08:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+javascript
+// Countdown Timer to next rocket launch (example date)
+var launchDate = new Date("Sep 10, 2025 14:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = launchDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("countdown").innerHTML = "Launched!";
+    }
+}, 1000);
